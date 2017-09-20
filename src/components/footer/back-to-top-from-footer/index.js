e6ff7c3ecdd3e997110a73057e12f65c9b27b18b
ThekhoN <bennetonss@hotmail.com>
2017-09-20 11:35:50 +0530

    add footer shell

--- src/components/footer/back-to-top-from-footer/index.js
+++ src/components/footer/back-to-top-from-footer/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import './style.css';
+// import './style.css';
+import styles from './style.css';
 const BackToTopFromFooter = () => (
-	<div className="back-to-top-from-footer">Back to top</div>
+	<div
+		className={`global-max-font-size back-to-top-from-footer ${styles.special}`}
+	>
+		Back to top
+	</div>
 );
 
 export default BackToTopFromFooter;
